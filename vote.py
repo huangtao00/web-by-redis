194e3f9a3aa4268d0b03590994d26fac570293d9
huangtao00 <397916230@qq.com>
2018-07-12 23:10:41 +0800

    added flash message show

--- vote.py
+++ vote.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 from flask import render_template
 from flask import request
+from flask import flash
 
 
 app=Flask(__name__)
-
+app.secret_key = 'some_secret'
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=="POST":
@@ -12,8 +13,8 @@ def index():
         keys=["title","author","link"]
         for key in keys:
             article[key]=request.form[key]
-        print (article)
-
+        flash("publish article {} sucessfully".format(article["title"]))
+        return redirect(url_for('index'))
     return render_template("index.html")
 
 
