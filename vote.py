6d6425493f55146b70b852a3856ae61420a62004
huangtao00 <397916230@qq.com>
2018-07-12 22:51:11 +0800

    added post rout

--- vote.py
+++ vote.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():
+    if request.method=="POST":
+        print ("you post me")
+        return ("you post me")
     return render_template("index.html")
 
 
