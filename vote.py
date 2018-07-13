f858bb4d8c80556e36e3047e48427ca85d5c5d88
huangtao00 <397916230@qq.com>
2018-07-13 14:15:47 +0800

    added page nav function

--- vote.py
+++ vote.py
@@ -12,17 +12,51 @@ def save_aritle_redis(article):
     for key, value in article.items():
         r.hset("article:"+str(counter),key,value)
 
-
-def get_ten_article(page=1):
+import math
+def get_articles(page=1,articelsperpage=6):
     r=redis.StrictRedis(host="localhost",port=6379,decode_responses=True)
     counter=r.get("counter")
-    if page*10>counter
-
+    if counter:
+        articlelist=[]
+        counter=int(counter)
+        allpages=math.ceil(counter/articelsperpage)
+        curpage=allpages-page+1
+        if  curpage==1: #no next page
+            #get article page*10-10+1 to counter artice
+            start,end=1,counter-(allpages-1)*articelsperpage+1
+        else: #have next page
+            #get articel page*10-10+1 to page*10+1
+            if curpage==allpages:
+                end=counter
+            else:  #11 11-4
+                end=counter-(page-1)*articelsperpage
+                print (counter)
+                print (end)
+            start=end-articelsperpage+1
+            end=end+1
+        #cur  8 7 6 5 4 3 2 1
+        if curpage<allpages:
+            previouspage=range(1,page)
+        else:
+            previouspage=False
+        if curpage>1:
+            nextpage=range(page+1,allpages+1)
+        else:
+            nextpage=False
+        for i in range(start,end):
+            article="article:"+str(i)
+            onearticle=r.hgetall(article)
+            onearticle["index"]=i
+            articlelist.append(onearticle)
+        articlelist.reverse()
+        return articlelist,nextpage,previouspage
 app=Flask(__name__)
 app.secret_key = 'some_secret'
 @app.route("/",methods=["GET","POST"])
-def index():
+@app.route("/page/<int:index>",methods=["GET","POST"])
+def index(index=1):
     if request.method=="POST":
+        #save article to redis
         article={}
         keys=["title","author","link","time"]
         now=datetime.datetime.now()
@@ -35,7 +69,10 @@ def index():
         save_aritle_redis(article)
         flash("发表文章\"{}\"成功！".format(article["title"]))
         return redirect(url_for('index'))
-    return render_template("index.html")
+    # show newest articles
+    allarticles,nextpage,prepage=get_articles(page=index)
+    pagenav=dict({"pre":prepage,"cur":index,"next":nextpage})
+    return render_template("index.html",articles=allarticles,pagenav=pagenav)
 
 
 
